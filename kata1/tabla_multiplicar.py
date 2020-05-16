c3a34f2dfd43554b10f1ea6ce69e4e553becb4bc
JuanmaGlez <smigol_82@yahoo.es>
2020-05-16 17:31:18 +0200

    repasos

--- kata1/tabla_multiplicar.py
+++ kata1/tabla_multiplicar.py
@@ -2,9 +2,15 @@
 enunciado
 """
 #tabla = 5
-tabla = int(input("Dime el númoro de la tabla "))
+#tabla = int(input("Dime el númoro de la tabla "))
+tabla = input("Dime el númoro de la tabla ")
+try:
+    tabla = int(tabla)
+#    if (isinstance(tabla, int)):
+        # range(11) -> range(0, 11, 1)  -> empieza por 0, y el numero llega hasta x-1
+    for i in range(11):
+        #print(i)
+        print(str(tabla) + " x " + str(i) + " = " + str(tabla * i))
+except:
+    print("Solo vale números")    
 
-# range(11) -> range(0, 11, 1)  -> empieza por 0, y el numero llega hasta x-1
-for i in range(11):
-    #print(i)
-    print(str(tabla) + " x " + str(i) + " = " + str(tabla * i))
